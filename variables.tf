0034c9c41fdb44f6c86135243ce54820421fe449
Maciej Michalski <5445923+spy86@users.noreply.github.com>
2025-01-27 12:54:28 +0000

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,53 +1,39 @@
-###########################
-# Common vars
-###########################
 variable "environment" {
-  description = "Variable that defines the name of the environment."
+  description = "Specifies the environment name, such as dev, test, or prod."
   type        = string
   default     = "dev"
 }
+
 variable "default_tags" {
-  description = "A mapping of tags to assign to the resource."
-  type        = map(any)
+  description = "A map of tags to be applied to all resources for consistent metadata and management."
+  type        = map(string)
   default = {
     "ManagedByTerraform" = "True"
   }
 }
+
 variable "region" {
-  description = "Region in which resources are deployed."
+  description = "The Azure region where resources will be deployed (e.g., 'weu' for West Europe)."
   type        = string
   default     = "weu"
 }
-############################
-#Resource gropus vars
-############################
+
 variable "resource_group_location" {
-  description = "Specifies the supported Azure location where the resource exists. Changing this forces a new resource to be created."
+  description = "The Azure region where the resource group exists. Modifying this will recreate the resource group."
+  type        = string
 }
+
 variable "resource_group_name" {
-  description = "The name of the resource group in which to create the network security group. Changing this forces a new resource to be created."
-}
-###############################
-#Network scurity group variable
-###############################
-variable "network_security_group_name" {
-  description = "Specifies the name of the network security group. Changing this forces a new resource to be created."
+  description = "The name of the Azure resource group where the network security group will be created. Changing this triggers the recreation of the resource."
   type        = string
 }
-variable "network_security_group_list" {
-  description = "List of security_rule objects representing security rules, as defined below."
-  type        = map(any)
-  default = {
-    "AllowHTTPAndHTTPS" = { priority = "1001", direction = "Inbound", access = "Allow", protocol = "Tcp", source_port_range = "*", destination_port_range = "80,443", source_address_prefix = "192.168.1.0/24", destination_address_prefix = "10.1.0.0/16" },
-  }
-}
-variable "associate_with_subnet" {
-  description = "Controls whether the Network Security Group should be associated with a subnet. When set to true, an association with the specified subnet is created; when set to false, no subnet association is established."
-  type        = bool
-  default     = false
-}
-variable "subnet_id" {
-  description = "The ID of the Subnet. Changing this forces a new resource to be created."
+
+variable "nsg_name" {
+  description = "The name of the network security group to be created."
   type        = string
-  default     = ""
+}
+
+variable "security_rules" {
+  description = "A list of security rules to apply to the network security group. Each rule should be a map containing the required properties. Refer to the documentation for details."
+  type        = list(map(string))
 }
